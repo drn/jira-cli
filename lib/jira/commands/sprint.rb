fad8998e5c4a40057b8f969d7d96bdc231e5930a
darrenli <darren@thanx.com>
2014-01-13 12:15:42 -0800

    Update to v0.1.2.

---
+++ lib/jira/commands/sprint.rb
@@ -0,0 +1,10 @@
+module Jira
+  class CLI < Thor
+
+    desc "sprint", "Lists all tickets in active sprint"
+    def sprint
+      # TODO
+    end
+
+  end
+end
--- lib/jira/constants.rb
+++ lib/jira/constants.rb
@@ -1,5 +1,5 @@
 module Jira
 
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 
 end
