2aa049096a429560497ea39ce856d4e9378791dd
darrenli <darren@thanx.com>
2014-01-19 00:17:04 -0800

    Add initial rspec tests.

--- jira-cli.gemspec
+++ jira-cli.gemspec
@@ -21,4 +21,5 @@ Gem::Specification.new do |s|
   s.add_dependency 'highline', '~> 1.6.20', '>= 1.6.20'
   s.add_dependency 'faraday', '~> 0.8.8', '>= 0.8.9'
   s.add_development_dependency 'pry-remote', '~> 0.1.7', '>= 0.1.7'
+  s.add_development_dependency 'rspec', '~> 2.14.1', '>= 2.14.1'
 end
---
+++ spec/jira/core_spec.rb
@@ -0,0 +1,23 @@
+require 'jira/core'
+
+describe Jira::Core do
+
+  context "setup" do
+    it "should call memoization functions" do
+      expect(Jira::Core).to receive(:url).once
+      expect(Jira::Core).to receive(:auth).once
+      Jira::Core.setup
+    end
+  end
+
+  context "url" do
+    it "should memoize " do
+      expect(Jira::Core.instance_variable_get(:@url)).to be(nil)
+      Jira::Core.stub(:read){ 'url' }
+      expect(Jira::Core.url).to be_an_instance_of(String)
+      expect(Jira::Core.instance_variable_get(:@url)).to_not be(nil)
+    end
+  end
+
+end
+
