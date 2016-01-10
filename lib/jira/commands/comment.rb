9c374087ade66444218acc302b66da5bcdc83df5
SDarayan <sdarayan.public@gmail.com>
2016-01-09 21:08:20 -0800

    comment.rb: Add command to update a comment
    A user can update a comment via "commentu" command.
    The user is prompted for a list of comments, then
    selects an index, and types a new string for the comment.
    
    Misc:
    - Refactored asking a user for a comment to method
      get_comment_body

--- lib/jira/commands/comment.rb
+++ lib/jira/commands/comment.rb
@@ -3,17 +3,11 @@ module Jira
 
     desc "comment", "Add a comment to the input ticket"
     def comment(ticket=Jira::Core.ticket)
-      comment = self.io.ask("Leave a comment for ticket #{ticket}")
-      if comment.strip.empty?
-        puts "No comment posted."
-      else
-        temp = comment.gsub(/\@[a-zA-Z]+/,'[~\0]')
-        temp = comment if temp.nil?
-        temp = temp.gsub('[~@','[~')
-        comment = temp if !temp.nil?
-        self.api.post("issue/#{ticket}/comment", { body: comment }) do |json|
-          puts "Successfully posted your comment."
-        end
+      comment = self.get_comment_body(ticket)
+      puts "No comment posted." and return if comment.empty?
+
+      self.api.post("issue/#{ticket}/comment", { body: comment }) do |json|
+        puts "Successfully posted your comment."
       end
     end
 
@@ -38,5 +32,56 @@ module Jira
       end
     end
 
+    desc "commentu", "Update a comment to the input ticket"
+    def commentu(ticket=Jira::Core.ticket)
+      comments(ticket) if self.io.agree("List comments for ticket #{ticket}")
+
+      index = self.get_comment_index("update")
+      puts "No comment updated." and return if index < 0
+
+      comment = self.get_comment_body(ticket)
+      puts "No comment updated." and return if comment.empty?
+
+      self.api.get("issue/#{ticket}") do |json|
+        comments = json['fields']['comment']['comments']
+        id = comments[index]['id']
+        self.api.put("issue/#{ticket}/comment/#{id}", { body: comment }) do |json|
+          puts "Successfully updated your comment."
+          return
+        end
+      end
+      puts "No comment updated."
+    end
+
+    protected
+
+      #
+      # Prompts the user for a comment body, strips it, then
+      # returns a substituted version of it
+      #
+      # @return comment [String] asked comment body
+      #
+      def get_comment_body(ticket)
+        comment = self.io.ask("Leave a comment for ticket #{ticket}").strip
+        temp = comment.gsub(/\@[a-zA-Z]+/,'[~\0]')
+        temp = comment if temp.nil?
+        temp = temp.gsub('[~@','[~')
+        comment = temp if !temp.nil?
+        comment
+      end
+
+      #
+      # Prompts the user for a comment index, then returns the
+      # comment index; failure is < 0
+      #
+      # @return index [Integer] asked comment index
+      #
+      def get_comment_index(description = "")
+        response = self.io.ask("Index for comment to #{description}").strip
+        return -1 if response.empty?
+        index = response.to_i
+        return -1 if index < 0
+        index
+      end
   end
 end
