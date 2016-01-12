adcf7a24722f835a47fb13875be544151a444026
SDarayan <sdarayan.public@gmail.com>
2016-01-11 16:44:41 -0800

    log.rb: Add command "logs"
    Users should be able to view worklogs for a ticket,
    so "logs" command was added to the cli.

--- lib/jira/commands/log.rb
+++ lib/jira/commands/log.rb
@@ -9,5 +9,26 @@ module Jira
       end
     end
 
+    desc "logs", "Lists the worklog of the input ticket"
+    def logs(ticket=Jira::Core.ticket)
+      self.api.get("issue/#{ticket}/worklog") do |json|
+        worklogs = json['worklogs']
+        if worklogs.count > 0
+          worklogs.each do |worklog|
+            author = worklog['author']['displayName']
+            time = Time.parse(worklog['updated'])
+            time_spent = worklog['timeSpent']
+
+            printf "[%2d]", worklogs.index(worklog)
+            puts "  #{Jira::Format.user(author)} @ "\
+                 "#{Jira::Format.time(time)}:\n"\
+                 "#{Jira::Format.comment(time_spent)}"
+          end
+        else
+          puts "There are no worklogs on ticket #{ticket}"
+        end
+      end
+    end
+
   end
 end
