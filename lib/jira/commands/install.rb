f77554ee2e47dc4019f92697525c5d17fd107da7
Darren Cheng <darren@thanx.com>
2016-01-22 00:35:22 -0800

    Refactor Command::Install.

--- lib/jira/commands/install.rb
+++ lib/jira/commands/install.rb
@@ -1,28 +1,53 @@
-#
-# Installation Script
-#
-
 module Jira
   class CLI < Thor
 
-    desc "install", "Guides the user through JIRA installation"
+    desc "install", "Guides the user through JIRA CLI installation"
     def install
-
-      inifile = IniFile.new(:comment => '#', :encoding => 'UTF-8', :filename => Jira::Core.cli_path)
-
-      url = self.io.ask("Enter your JIRA URL:")
-      username = self.io.ask("Enter your JIRA username:")
-      password = self.io.mask("Enter your JIRA password:")
-
-      inifile[:global] = {
-        url: url,
-        username: username,
-        password: password
-      }
-      inifile.write
-
-      Jira::Core.send(:discard_memoized)
+      Command::Install.new.run
     end
 
+  end
+
+  module Command
+    class Install < Base
+
+      def run
+        io.say('Please enter your JIRA information.')
+        inifile[:global] = params
+        inifile.write
+        Jira::Core.send(:discard_memoized)
+      end
+
+    private
+
+      def params
+        {
+          url:      url,
+          username: username,
+          password: password
+        }
+      end
+
+      def url
+        io.ask("JIRA URL:")
+      end
+
+      def username
+        io.ask("JIRA username:")
+      end
+
+      def password
+        io.mask("JIRA password:")
+      end
+
+      def inifile
+        @inifile ||= IniFile.new(
+          comment:  '#',
+          encoding: 'UTF-8',
+          filename: Jira::Core.cli_path
+        )
+      end
+
+    end
   end
 end
