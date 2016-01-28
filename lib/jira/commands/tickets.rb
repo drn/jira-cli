db341db6423c8d2e0b235a4a1c7e991701a4839b
SDarayan <sdarayan.public@gmail.com>
2016-01-27 22:41:54 -0800

    tickets.rb: Refactor tickets command

--- lib/jira/commands/tickets.rb
+++ lib/jira/commands/tickets.rb
@@ -1,11 +1,29 @@
+require_relative '../command'
+
 module Jira
   class CLI < Thor
 
     desc "tickets", "List the tickets of the input username"
     def tickets(username=Jira::Core.username)
-      self.api.get("search?jql=assignee=#{username}") do |json|
-        issues = json['issues']
-        if issues.count > 0
+      Command::Tickets.new(username).run
+    end
+  end
+
+  module Command
+    class Tickets < Base
+
+      attr_accessor :username
+
+      def initialize(username)
+        self.username = username
+      end
+
+      def run
+        return if username.empty?
+        return if metadata.empty?
+
+        issues = metadata['issues']
+        if !issues.nil? and issues.count > 0
           issues.each do |issue|
             ticket = issue['key']
 
@@ -16,7 +34,13 @@ module Jira
           puts "There are no tickets for username #{username}."
         end
       end
-    end
 
+      private
+
+      def metadata
+        @metadata ||= api.get("search?jql=assignee=#{username}")
+      end
+
+    end
   end
 end
