fada9bfda1c4f6fad62322cb4b9e53a5ad4b8507
SDarayan <sdarayan.public@gmail.com>
2016-01-27 21:34:30 -0800

    vote.rb: Refactor vote command

--- lib/jira/commands/vote.rb
+++ lib/jira/commands/vote.rb
@@ -1,20 +1,69 @@
+require_relative '../command'
+
 module Jira
   class CLI < Thor
 
     desc "vote", "Vote against the input ticket"
     def vote(ticket=Jira::Core.ticket)
-      self.api.post("issue/#{ticket}/votes", Jira::Core.username) do |json|
-        puts "Successfully voted against ticket #{ticket}"
-        return
-      end
-      puts "No vote."
+      Command::Vote.new(ticket).run
     end
 
     desc "votes", "List the votes of the input ticket"
     def votes(ticket=Jira::Core.ticket)
-      self.api.get("issue/#{ticket}/votes") do |json|
-        voters = json['voters']
-        if voters.count > 0
+      Command::Votes.new(ticket).run
+    end
+
+    desc "unvote", "Unvote against the input ticket"
+    def unvote(ticket=Jira::Core.ticket)
+      Command::Unvote.new(ticket).run
+    end
+
+  end
+
+  module Command
+    class Vote < Base
+
+      attr_accessor :ticket
+
+      def initialize(ticket)
+        self.ticket = ticket
+      end
+
+      def run
+        return if ticket.empty?
+
+        api.post "issue/#{ticket}/votes",
+          params:  Jira::Core.username,
+          success: on_success,
+          failure: on_failure
+      end
+
+      private
+
+      def on_success
+        ->{ puts "Successfully added vote to ticket #{ticket}" }
+      end
+
+      def on_failure
+        ->{ puts "No vote." }
+      end
+
+    end
+
+    class Votes < Base
+
+      attr_accessor :ticket
+
+      def initialize(ticket)
+        self.ticket = ticket
+      end
+
+      def run
+        return if ticket.empty?
+        return if metadata.empty?
+
+        voters = metadata['voters']
+        if !voters.nil? and voters.count > 0
           voters.each do |voter|
             displayName = voter['displayName']
 
@@ -25,17 +74,41 @@ module Jira
           puts "There are no votes on ticket #{ticket}."
         end
       end
-    end
 
-    desc "unvote", "Unvote against the input ticket"
-    def unvote(ticket=Jira::Core.ticket)
-      username = Jira::Core.username
-      self.api.delete("issue/#{ticket}/votes?username=#{username}") do |json|
-        puts "Successfully unvoted against ticket #{ticket}"
-        return
+      private
+
+      def metadata
+        @metadata ||= api.get("issue/#{ticket}/votes")
       end
-      puts "No unvote."
     end
 
+    class Unvote < Base
+
+      attr_accessor :ticket
+
+      def initialize(ticket)
+        self.ticket = ticket
+      end
+
+      def run
+        return if ticket.empty?
+
+        username = Jira::Core.username
+        api.delete "issue/#{ticket}/votes?username=#{username}",
+          success: on_success,
+          failure: on_failure
+      end
+
+      private
+
+      def on_success
+        ->{ puts "Successfully removed vote from ticket #{ticket}" }
+      end
+
+      def on_failure
+        ->{ puts "No unvote." }
+      end
+
+    end
   end
 end
